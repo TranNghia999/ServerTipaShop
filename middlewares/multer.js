f136d8cdc78cf4315fa1f606eebd760513685b80
Anh Nghia <thaihoa.program@gmail.com>
2025-12-12 15:10:56 +0700

    Thu lan 11

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,16 +1,30 @@
-import multer from 'multer';
-import fs from 'fs';
+// Code dùng cho local  
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(null, `${Date.now()}_${file.originalname}`);
-    // imagesArr.push(`${Date.now()}_${file.originalname}`)
-  },
+// import multer from 'multer';
+// import fs from 'fs';
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "uploads");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, `${Date.now()}_${file.originalname}`);
+//     // imagesArr.push(`${Date.now()}_${file.originalname}`)
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
+// export default upload;
+
+// Code dùng cho publish server ( Phát viết )
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB (tuỳ chọn)
 });
-
-const upload = multer({ storage: storage });
 
 export default upload;
